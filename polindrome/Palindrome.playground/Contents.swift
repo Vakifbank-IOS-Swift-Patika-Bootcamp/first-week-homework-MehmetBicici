7e4026b710e7c88149e3ed8ad92f84bf3d693143
Mehmet Bicici <115979022+mehmetBcc@users.noreply.github.com>
2022-11-06 14:42:48 +0300

    Fixed some bugs related to palindrome question.

--- polindrome/Palindrome.playground/Contents.swift
+++ polindrome/Palindrome.playground/Contents.swift
@@ -2,15 +2,17 @@ import Foundation
 
 
 func isPolindrom(word: String) -> Bool {
+    
     var startIdx = 0
     var endIdx = word.count - 1
     var letterCharForward: Character
     var letterCharBack: Character
-    while(startIdx < word.count) {
-        var forwardIdx = word.index(word.startIndex, offsetBy: startIdx)
-        var backIdx = word.index(word.startIndex, offsetBy: endIdx)
-        letterCharForward = word[forwardIdx]
-        letterCharBack = word[backIdx]
+    let upperCaseWord = word.lowercased()
+    while(startIdx < upperCaseWord.count) {
+        let forwardIdx = upperCaseWord.index(upperCaseWord.startIndex, offsetBy: startIdx)
+        let backIdx = upperCaseWord.index(upperCaseWord.startIndex, offsetBy: endIdx)
+        letterCharForward = upperCaseWord[forwardIdx]
+        letterCharBack = upperCaseWord[backIdx]
         if letterCharBack != letterCharForward {
             return false
         }
@@ -23,9 +25,9 @@ func isPolindrom(word: String) -> Bool {
 /*
  
  - If you want to try the function I wrote above, I'll put some sample palindrome words for you.
- - Polindrome Words : bib, nun, madam, racecar, cıvıc,deıfıed, level.
+ - Polindrome Words : bib, nun, madam, racecar, cıvıc,deıfıed, level, DogeeseseeGod.
  - Not Polindrome Words : try, not, give, up, you, can, do, it, whatever, happens
  */
-print(isPolindrom(word: "whatever"))
+print(isPolindrom(word: "tacocattacocaT"))
 
 
